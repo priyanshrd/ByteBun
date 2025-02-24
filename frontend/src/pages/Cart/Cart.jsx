6ecf166deb7754f80ac50ca411dfce9c3958fd04
Priyansh Rajiv Dhotar <priyansh17rd@gmail.com>
2025-02-25 03:04:20 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from 'react';
 import './Cart.css';
-import { StoreContext } from '../../Context/StoreContext';
+import { StoreContext } from '../../context/StoreContext';
 import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {

ee382e072c3d974f64a62a1da85889d5dd196abc
Priyansh Rajiv Dhotar <priyansh17rd@gmail.com>
2025-02-25 03:42:56 +0530

    Update assets.js

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -96,7 +96,7 @@ import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
 
 export const currency = '₹'
-export const url = 'https://bytebun.onrender.com/'
+export const url = 'https://bytebun.onrender.com'
 export const assets = {
     logo,
     basket_icon,

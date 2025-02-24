7dedbcf76cbd507904cc0ce56b79424748f8f303
Priyansh Rajiv Dhotar <priyansh17rd@gmail.com>
2025-02-25 03:42:33 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://bytebun.onrender.com/";
+    const url = "https://bytebun.onrender.com";
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")

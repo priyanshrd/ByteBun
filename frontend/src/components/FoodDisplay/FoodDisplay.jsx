87a5478b47c0c5c58fd8eb9378a03469b4f74cd1
Priyansh Rajiv Dhotar <priyansh17rd@gmail.com>
2025-02-25 03:05:45 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import './FoodDisplay.css';
 import FoodItem from '../FoodItem/FoodItem';
-import { StoreContext } from '../../Context/StoreContext';
+import { StoreContext } from '../../context/StoreContext';
 
 const FoodDisplay = ({ category, selectedRestaurant }) => {
   const { food_list } = useContext(StoreContext);

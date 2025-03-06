3ef50b723e21dbe7789bfd4d255abadf4103e1aa
Priyansh Rajiv Dhotar <priyansh17rd@gmail.com>
2025-03-06 08:17:23 +0000

    double solved

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -12,21 +12,32 @@ const FoodDisplay = ({ category, selectedRestaurant }) => {
         {food_list.map((item) => {
           // Filter based on category or selected restaurant
           if (
-            (category && ( category === item.category)) ||
-            (selectedRestaurant && selectedRestaurant === item.restaurant_id)
-          ) {
-            return (
-              <FoodItem
-                key={item._id}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-                rating={item.rating} // Pass the rating prop here
-              />
-            );
-          } else 
+            (category && ( category === item.category)) ){
+              return (
+                <FoodItem
+                  key={item._id}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                  rating={item.rating} // Pass the rating prop here
+                />
+              );
+            }else if (selectedRestaurant && selectedRestaurant === item.restaurant_id){
+              return (
+                <FoodItem
+                  key={item._id}
+                  id={item._id}
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                  image={item.image}
+                  rating={item.rating} // Pass the rating prop here
+                />
+              );
+            }
+           else 
           return null; // Return null for items that don't match the category or restaurant
         })}
       </div>
